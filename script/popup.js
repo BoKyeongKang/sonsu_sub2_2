dd1213c7c0209244b5c9afe995dcb9ceebbb0c17
BoKyeongKang <99226703+BoKyeongKang@users.noreply.github.com>
2022-02-14 13:21:58 +0900

    Add files via upload

---
+++ script/popup.js
@@ -0,0 +1,30 @@
+$(function(){
+  $(".wrap1 a").click(function(){
+    $(".modal1").show();//.fadeIn(); 서서히 나타남.
+  });
+
+  $(".wrap2 a").click(function(){
+    $(".modal2").show();//.fadeIn(); 서서히 나타남.
+  });
+
+  $(".wrap3 a").click(function(){
+    $(".modal3").show();//.fadeIn(); 서서히 나타남.
+  });
+
+  $(".wrap4 a").click(function(){
+    $(".modal4").show();//.fadeIn(); 서서히 나타남.
+  });
+
+  $(".wrap5 a").click(function(){
+    $(".modal5").show();//.fadeIn(); 서서히 나타남.
+  });
+
+  $(".wrap6 a").click(function(){
+    $(".modal6").show();//.fadeIn(); 서서히 나타남.
+  });
+
+
+$(".popup a").click(function(){
+  $(".popup").hide();//.fadeIn(); 서서히 사라짐.
+});
+});
